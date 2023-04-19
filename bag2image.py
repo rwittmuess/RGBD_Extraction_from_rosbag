e1889b17ed584c466ac4f65280affd4f9965a603
rwittmuess <wittmuess.richard@gmail.com>
2023-04-18 21:00:08 -0700

    Finish MVP-bag2img

--- bag2image.py
+++ bag2image.py
@@ -14,16 +14,28 @@ from sensor_msgs.msg import Image
 from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
+from bagpy import bagreader
+import pandas as pd
+import seaborn as sea
 
 
-def GetImageData(location_bag_file, img_topic = '/d455/depth/image_rect_raw'):
+def getTopics(location_bag_file):
+    '''
+    TODO: Description
+    '''
+    bag = rosbag.Bag(location_bag_file)
+    topics = list(bag.get_type_and_topic_info()[1].keys())
+    return(topics)
+
+
+def getImageData(location_bag_file, img_topic = '/d455/depth/image_rect_raw'):
     '''
     First part inspired by: http://wiki.ros.org/rosbag/Code%20API 
 
     location_bag_file   - Path to the .bag file. 
                           If it is in the same directory just the name of the bag file.
     img_topic           - ROS topic of the camera.
-                          Can be found by using TODO: functionxyz
+                          Can be found out using: getTopics(location_bag_file)
 
     '''
     bag = rosbag.Bag(location_bag_file)
@@ -33,7 +45,12 @@ def GetImageData(location_bag_file, img_topic = '/d455/depth/image_rect_raw'):
     bag.close()
     return image_data
 
-def DispImg(msg):
+def dispImg(msg):
+    '''
+    TODO: Description
+    
+    '''
+
     #(480, 848, 2)
     im = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
     new_im1 = im[:,:,0]
@@ -46,15 +63,4 @@ def DispImg(msg):
     img = Image.fromarray(new_im2, 'L')
     plt.imshow(img,cmap='gray')
     plt.show()
-
-'''
->>> import bag2image as i
->>> k = []
->>> k = i.GetImageData('test3.bag')
->>> i.DispImg(k[0])
-'''
-
-
-
-
 
